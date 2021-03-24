3da9419d48d12d5540457a60130109abc23299dc
“Lingsong <lingsongmeng@ufl.edu>
2021-03-24 04:46:44 -0400

    version 1

---
+++ R/BMAseq.multi.postprob.Laplace.MSout.R
@@ -0,0 +1,74 @@
+##' Calculation of posterior model probability for each gene in multivariate analysis using BMAseq approach
+##'
+##' Calculation of posterior model probability for each gene in multivariate analysis on RNA-seq count data using BMAseq approach
+##' @title BMAseq.multi.postprob
+##' @param dat.expr.counts RNA-seq count data matrix (rows for genes and columns for subjects).
+##' @param dat.pheno Phenotypic data matrix (rows for subjects and columns for variables).
+##' @param model.space Model space
+##' @param cut.BF Bayes factor criterion, default value is 1.
+##' @return A list consisting of
+##' \item{dat.expr.logcpm}{Normalized RNA-seq data matrix (rows for genes and columns for subjects).}
+##' \item{weights}{Estimated voom weights.}
+##' \item{dat.pheno.new}{Phenotypic data matrix including new interaction variables, will be same as input dat.pheno if interaction=NULL.}
+##' \item{model.space}{Model space including all possible models.}
+##' \item{post.modelprob}{Posterior model probability for each gene.}
+##' @export
+##' @author Lingsong Meng
+
+
+BMAseq.multi.postprob.Laplace.MSout <- function(dat.expr.counts, dat.pheno, model.space, cut.BF = 1) {
+    
+    
+    if (sum(colnames(dat.expr.counts) != rownames(dat.pheno)) > 0) 
+        stop("Two datasets dat.expr.counts and dat.pheno must be matched by subjects.")
+    
+    n.var <- length(var.pool)
+    n.gene <- nrow(dat.expr.counts)
+    
+    
+    # estimate voom weights
+    y0 <- voom(dat.expr.counts)
+    input.dat.expr <- y0$E
+    input.weights <- y0$weights
+    
+    
+    # build model space
+    input.model.space <- model.space
+    input.dat.pheno <- dat.pheno
+    
+    
+    # calculate Bayes factor
+    out.logbf <- vapply(1:length(input.model.space), function(i) {
+        print(paste0(i, ". ", input.model.space[i]))
+        design <- model.matrix(formula(input.model.space[i]), data = input.dat.pheno)
+        colnames(design)[1] <- "Int"
+        vapply(1:n.gene, function(k) Bayesfactor.Laplace(design, input.dat.expr[k, ], input.weights[k, ]), FUN.VALUE = as.double(1))
+    }, FUN.VALUE = as.double(1:n.gene))
+    
+    
+    
+    # obtain prior model probability
+    logbf.max <- t(apply(out.logbf, 1, function(x) ifelse(x == max(x) & x > log(cut.BF), 1, 0)))
+    prior.modelprob <- apply(logbf.max, 2, sum)/n.gene
+    pm.est <- prior.modelprob
+    n.model <- length(prior.modelprob)
+    pm.prior0 <- pm.prior1 <- rep(0.5/(n.model - 1), n.model - 1)
+    alpha <- 0.2
+    for (j in 1:30) {
+        denom <- t(apply(out.logbf[, -1], 1, function(y) exp(y - max(y) - log((1 - sum(pm.prior0))/max(exp(y)) + sum(exp(y + log(pm.prior0) - max(y)))))))
+        pm.prior1 <- pm.est[-1]/apply(denom, 2, mean)
+        pm.prior0 <- pm.prior0 + (pm.prior1 - pm.prior0) * alpha
+    }
+    prior.modelprob <- c(1 - sum(pm.prior0), pm.prior0)
+    
+    
+    # calculate posterior model probability
+    post.modelprob <- t(apply(out.logbf, 1, function(y) exp(y + log(prior.modelprob) - max(y) - log(sum(exp(y + log(prior.modelprob) - max(y)))))))
+    rownames(post.modelprob) <- rownames(dat.expr.counts)
+    colnames(post.modelprob) <- input.model.space
+    
+    
+    
+    return(list(dat.expr.logcpm = input.dat.expr, weights = input.weights, model.space = input.model.space, dat.pheno.new = input.dat.pheno, post.modelprob = post.modelprob))
+}
+
